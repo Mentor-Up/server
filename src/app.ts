a1e07cc1785ce40ffcb5a188f828bb16be478d28
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-07-22 11:10:26 -0400

    add error middleware

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ const app = express();
 import favicon from 'express-favicon';
 import logger from 'morgan';
 import session from 'express-session';
+import errorHandlerMiddleware from './middleware/error-handler';
+import notFoundMiddleware from './middleware/not-found'
 
 
 // middleware
@@ -15,6 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware)
 
 // routes
 // app.use('/api/v1',);
