e2d39f835a567009f309d8b10506d7da65d59f17
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-07-22 10:24:17 -0400

    adding errors handling

---
+++ src/errors/bad-request.ts
@@ -0,0 +1,13 @@
+import CustomAPIError from './custom-api';
+
+class BadRequestError extends CustomAPIError {
+    statusCode: number;
+
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+export default BadRequestError
---
+++ src/errors/unauthorized.ts
@@ -0,0 +1,12 @@
+import CustomAPIError from './custom-api';
+
+class UnauthorizedError extends CustomAPIError {
+    statusCode: number;
+
+    constructor(message:string) {
+        super(message);
+        this.statusCode = 403;
+      }
+}
+
+export default UnauthorizedError
