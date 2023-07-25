2e7e91cb4b9d2aa393eeb63fb7a90877c80cd837
cdpaiva <cdpaivaa@gmail.com>
2023-07-25 15:41:37 -0400

    Fixes syntax error

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
-import cors from 'cors';
-import 'express-async-errors';
-import express from 'express';
+import cors from "cors";
+import "express-async-errors";
+import express from "express";
 const app = express();
-import favicon from 'express-favicon';
-import logger from 'morgan';
+import favicon from "express-favicon";
+import logger from "morgan";
 import cookieParser from "cookie-parser";
-import session from 'express-session';
-import { node_env } from './config';
-import compression from 'compression'
-import rateLimiter from 'express-rate-limit';
-import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
-import errorHandlerMiddleware from './middleware/error-handler';
-import notFoundMiddleware from './middleware/not-found'
+import session from "express-session";
+import { node_env } from "./config";
+import compression from "compression";
+import rateLimiter from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import errorHandlerMiddleware from "./middleware/error-handler";
+import notFoundMiddleware from "./middleware/not-found";
 import authMiddleware from "./middleware/authentication";
 
 import authRouter from "./routes/auth";
 
 app.use(
-    rateLimiter({
-      windowMs: 15 * 60 * 1000,
-      max: 60,
-    })
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
 );
 app.use(helmet());
 app.use(compression());
@@ -31,22 +31,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-if (node_env === 'development') {
-  app.use(logger('dev'));
+if (node_env === "development") {
+  app.use(logger("dev"));
 } else {
   app.use(
-      logger(
+    logger(
       ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :response-time ms',
-      { stream: { write: (message) => console.log('info', message.trim(), { tags: ['http'] }) } }
+      {
+        stream: {
+          write: (message) =>
+            console.log("info", message.trim(), { tags: ["http"] }),
+        },
+      }
     )
   );
-app.use(express.static('public'))
-app.use(favicon(__dirname + '/public/favicon.ico'));
+}
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/testAuth", authMiddleware, (req, res) => res.send("OK!"));
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 export default app;
