d1c771607d7e758da42e76c0660768cdd8c75fc5
cdpaiva <cdpaivaa@gmail.com>
2023-07-26 14:02:45 -0400

    Uses custom errors instead of plain status codes

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,9 +2,18 @@ import User from "../models/User";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { jwtPayload } from "../middleware/authentication";
+import { BadRequestError, UnauthenticatedError } from "../errors";
+import {
+  ACCESS_TOKEN_EXPIRATION,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+} from "../config";
 
 const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("Missing credentials");
+  }
   const user = await User.create({ name, email, password });
 
   const token = user.createJWT();
@@ -27,17 +36,17 @@ const register = async (req: Request, res: Response) => {
 const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.sendStatus(404);
+    throw new BadRequestError("Missing credentials");
   }
 
   const user = await User.findOne({ email });
   if (!user) {
-    return res.sendStatus(401);
+    throw new UnauthenticatedError("Invalid credentials");
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    return res.sendStatus(401);
+    throw new UnauthenticatedError("Invalid credentials");
   }
   const token = user.createJWT();
   const refreshToken = user.createRefreshToken();
@@ -59,36 +68,24 @@ const login = async (req: Request, res: Response) => {
 const refreshToken = async (req: Request, res: Response) => {
   const cookies = req.cookies;
   if (!cookies?.token) {
-    return res.sendStatus(401);
+    throw new UnauthenticatedError("Invalid token");
   }
 
   const refreshToken = cookies.token;
 
   const user = await User.findOne({ refreshToken });
   if (!user) {
-    return res.sendStatus(401);
+    throw new UnauthenticatedError("Invalid token");
   }
 
-  if (!process.env.REFRESH_TOKEN_SECRET) {
-    return new Error("The .env file must have a REFRESH_TOKEN_SECRET key.");
-  }
-  if (!process.env.ACCESS_TOKEN_SECRET) {
-    return new Error("The .env file must have an ACCESS_TOKEN_SECRET key.");
-  }
-
-  const payload = jwt.verify(
-    refreshToken,
-    process.env.REFRESH_TOKEN_SECRET
-  ) as jwtPayload;
+  const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET!) as jwtPayload;
   if (!payload || payload.name !== user.name) {
     return res.sendStatus(403);
   }
 
-  const token = jwt.sign(
-    { username: payload.name },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION }
-  );
+  const token = jwt.sign({ username: payload.name }, ACCESS_TOKEN_SECRET!, {
+    expiresIn: ACCESS_TOKEN_EXPIRATION,
+  });
 
   return res.status(200).json({ token });
 };
--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express-serve-static-core";
+import { UnauthenticatedError } from "../errors";
+import { ACCESS_TOKEN_SECRET } from "../config";
 
 export interface jwtPayload {
   userId: string;
@@ -10,25 +12,18 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader?.startsWith("Bearer ")) {
-    return res.sendStatus(401);
+    return new UnauthenticatedError("Invalid token");
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-    if (!process.env.ACCESS_TOKEN_SECRET) {
-      throw new Error("The .env file must have a ACCESS_TOKEN_SECRET key.");
-    }
-
-    const payload = jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET
-    ) as jwtPayload;
+    const payload = jwt.verify(token, ACCESS_TOKEN_SECRET!) as jwtPayload;
 
     req.user = { userId: payload.userId };
     next();
   } catch (err) {
-    return res.sendStatus(401);
+    return new UnauthenticatedError("Invalid token");
   }
 };
 
