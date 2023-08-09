9a9882533402717a208784ed0ceebd579f1a3345
akabishau <aleksey.kabishau@icloud.com>
2023-08-08 21:30:36 -0400

    test formatting config

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,14 +1,19 @@
-import { createSession, getAllSession, getSession, updateSession, deleteSession  } from '../controllers/session';
+import {
+  createSession,
+  getAllSession,
+  getSession,
+  updateSession,
+  deleteSession,
+} from '../controllers/session';
 import express from 'express';
 
 const router = express.Router();
 
-router.route("/").get(getAllSession).post(createSession);
+router.route('/').get(getAllSession).post(createSession);
 router
-  .route("/:sessionId")
+  .route('/:sessionId')
   .get(getSession)
   .patch(updateSession)
   .delete(deleteSession);
-
 
 export default router;
