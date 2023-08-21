d58fcd2d701e0fde57b4e17533af5bc554a94170
akabishau <aleksey.kabishau@icloud.com>
2023-08-21 11:45:11 -0400

    update direct register logic and clean up

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,8 +1,9 @@
 import { NotFoundError } from '../errors';
 import User, { IUser } from '../models/User';
+import getConfirmationCode from '../utils/getConfirmationCode';
 
 interface IUserProfile {
-  _id: string;
+  id: string;
   name: string;
   email: string;
   role: string[];
@@ -10,12 +11,13 @@ interface IUserProfile {
   slackId?: string;
   avatarUrl?: string;
   isActivated?: boolean;
+  confirmationCode?: string;
 }
 
 class UserService {
   private generateUserProfile(user: IUser): IUserProfile {
     return {
-      _id: user._id,
+      id: user._id,
       name: user.name,
       email: user.email,
       role: user.role,
@@ -26,19 +28,6 @@ class UserService {
     };
   }
 
-  // I have users and cohort -> result all users are added to the cohort
-  // I need to check if the cohort exists - controller
-  // I need to check if the users are already registered
-  // I need to check if the users are already registered in the cohort
-  // I need to create the users
-  // I need to add the users to the cohort
-
-  // create user as separate function
-  // add to cohort as separate function
-  // register user = create user + add to cohort
-  // available input from controller: user + cohort
-  // cohort validation in controller for now
-
   async createUser(userData: Partial<IUser>): Promise<IUser> {
     return await User.create(userData);
   }
@@ -48,50 +37,17 @@ class UserService {
     return !!user;
   }
 
-  // isRegisteredInCohort - ? do we need to provide the feedback?
-  // or just make sure the user is added to the cohort
-  async addUserToCohort(
-    userId: string,
-    cohortId: string
-  ): Promise<IUser | null> {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      {
-        $addToSet: { cohorts: cohortId },
-      },
-      { new: true, runValidators: true }
-    );
-    console.log('addtoCohort', user);
-    return user;
-  }
-
-  async registerUser(
-    userData: Partial<IUser>,
-    cohort: string
-  ): Promise<IUser | null> {
-    const user = await User.create(userData);
-    const registerUser = await this.addUserToCohort(user._id, cohort);
-    return registerUser;
-  }
-  ////
-  ////
-  ////
-
   async registerDirectUser(
     userData: Partial<IUser>
   ): Promise<IUserProfile | null> {
     const user = await User.create({
       ...userData,
       isActivated: true,
-      confirmationCode: '111111',
-      cohorts: [],
+      confirmationCode: getConfirmationCode(),
     });
 
     return this.generateUserProfile(user);
   }
-
-  ///
-  ////
 
   async getUser(userId: string): Promise<IUserProfile | null> {
     const user = await User.findById(userId).populate('cohorts', '_id name');
