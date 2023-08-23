1975f4a18c5e93c6e0ce2817b0d43398e668e4d6
akabishau <aleksey.kabishau@icloud.com>
2023-08-23 11:23:34 -0400

    update user route to user restrict middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middleware/authentication';
-import { isAdmin } from '../middleware/isAdmin';
 import { getUsers, getUser, updateUser } from '../controllers/user';
+import restrict from '../middleware/authorizeRole';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getUsers);
 router.get('/:userId', authMiddleware, getUser);
-router.patch('/:userId', authMiddleware, isAdmin, updateUser);
+router.patch('/:userId', authMiddleware, restrict('admin'), updateUser);
 
 export default router;
