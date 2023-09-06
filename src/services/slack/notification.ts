33a5f0d629e5c7d2bd39b67bfaf4612141a1d954
akabishau <aleksey.kabishau@icloud.com>
2023-09-05 23:21:50 -0400

    add retries to the service

--- src/services/slack/notification.ts
+++ src/services/slack/notification.ts
@@ -1,29 +1,40 @@
 import { slackWebClient } from '../../utils/slack/slackWebClient';
 
 class SlackNotificationService {
+  private async postWithRetry(
+    channelId: string,
+    message: string,
+    retries: number = 3
+  ): Promise<string | null> {
+    for (let i = 0; i < retries; i++) {
+      try {
+        const response = await slackWebClient.chat.postMessage({
+          channel: channelId,
+          text: message,
+        });
+
+        if (response.ts) {
+          console.log('Message sent to channel:', response.ts);
+          return response.ts;
+        } else {
+          console.warn('Unexpected Slack response format. Missing timestamp.');
+        }
+      } catch (error) {
+        console.error(
+          `Error posting message to channel. Attempt ${i + 1} of ${retries}:`,
+          error
+        );
+      }
+    }
+    console.error(`Failed to post message after ${retries} attempts.`);
+    return null;
+  }
+
   async postToChannel(
     channelId: string,
     message: string
   ): Promise<string | null> {
-    try {
-      console.log('Posting message to channel...');
-      const response = await slackWebClient.chat.postMessage({
-        channel: channelId,
-        text: message,
-      });
-
-      console.log('Message sent to channel:', response.ts);
-
-      if (response.ts) {
-        return response.ts;
-      } else {
-        console.warn('Unexpected Slack response format. Missing timestamp.');
-        return null;
-      }
-    } catch (error) {
-      console.error('Error posting message to channel:', error);
-      return null;
-    }
+    return this.postWithRetry(channelId, message);
   }
 }
 
