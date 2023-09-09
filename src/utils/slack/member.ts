b619538abf297900829eac1dc342b9fbd444ee68
akabishau <aleksey.kabishau@icloud.com>
2023-09-09 19:22:26 -0400

    refactor out extract member profile function into helper

--- src/utils/slack/member.ts
+++ src/utils/slack/member.ts
@@ -1,5 +1,7 @@
+// TODO: Use Bolt instead of WebClient
 import { slackWebClient, WebAPICallResult } from './slackWebClient';
 import { SlackMember } from '../../slack/types/member';
+import { extractMemberProfile } from '../../slack/utils/memberHelper';
 
 async function fetchChannelMembers(channelId: string): Promise<string[]> {
   try {
@@ -20,28 +22,6 @@ async function fetchMemberData(userId: string): Promise<WebAPICallResult> {
     console.error('Error fetching user details:', error);
     throw error;
   }
-}
-
-function extractMemberProfile(userData: WebAPICallResult): SlackMember {
-  const user = userData.user as any;
-  const profile = user?.profile || {};
-  return {
-    id: user.id,
-    email: profile.email,
-    name: profile.real_name,
-    firstName: profile.first_name,
-    lastName: profile.last_name,
-    title: profile.title,
-    displayName: profile.display_name,
-    avatarUrl: profile.image_original,
-    isAdmin: user.is_admin || false,
-    isBot: user.is_bot,
-    isEmailConfirmed: user.is_email_confirmed,
-    teamId: user.team,
-    tz: user.tz,
-    tz_label: user.tz_label,
-    tz_offset: user.tz_offset,
-  };
 }
 
 export async function fetchChannelMembersDetails(
