f47a7b2137295f70c07f5fd79374d3e6ac30d0f7
akabishau <aleksey.kabishau@icloud.com>
2023-09-10 14:39:43 -0400

    improve cohort view structure

--- src/slack/views/home/cohort.ts
+++ src/slack/views/home/cohort.ts
@@ -1,9 +1,13 @@
-import { Blocks } from 'slack-block-builder';
+import { Blocks, Md } from 'slack-block-builder';
 import { ThisWeekCohortSessions } from '../../types/cohortSession';
 
 export const buildCohortSection = (cohort: ThisWeekCohortSessions) => {
+  const channel = cohort.slackId ? Md.channel(cohort.slackId) : cohort.name;
   return [
-    Blocks.Header().text(cohort.name + ': ' + cohort.week),
+    Blocks.Header().text(cohort.week),
+    Blocks.Section().text(
+      Md.emoji('slack') + ' ' + channel + ': ' + cohort.type
+    ),
     Blocks.Divider(),
   ];
 };
