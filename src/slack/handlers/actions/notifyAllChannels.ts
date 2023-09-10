e91fb437f8414b010c8cd50de09985c1ed46a96b
akabishau <aleksey.kabishau@icloud.com>
2023-09-10 16:46:09 -0400

    remove hardcoded channel and user id

--- src/slack/handlers/actions/notifyAllChannels.ts
+++ src/slack/handlers/actions/notifyAllChannels.ts
@@ -7,38 +7,34 @@ import SlackNotificationService from '../../services/notification';
 export const handleNotifyAllChannels = async (
   args: SlackActionMiddlewareArgs
 ) => {
-  const { ack } = args;
+  const { ack, body } = args;
   await ack();
-  console.log('Notify All button was clicked!');
   try {
     const weekData = await sessionsDataService.getThisWeekSessions(
-      'U05MVFX85EW'
+      body.user.id
     );
     const messages = MessageBuilderService.weeklySessions(weekData.cohorts);
 
-    const channelId = 'C05PG2K45EE'; // hedgehog;
-    const responses = await Promise.all(
+    await Promise.all(
       messages.map(async (message) => {
         const response = await SlackNotificationService.postToChannel(
-          channelId, // message.channelId,
+          message.channelId,
           message.text,
           message.blocks
         );
 
         if (response) {
-          // return { success: true, channelId: message.channelId, response };
-          return { success: true, channelId: channelId, response };
+          return { success: true, channelId: message.channelId, response };
         } else {
           return {
             success: false,
-            channelId: channelId, //message.channelId,
+            channelId: message.channelId,
             error: 'Failed after retries',
           };
         }
       })
     );
-    console.log('responses:', responses);
   } catch (error) {
-    console.error('Error in handleWeeklySessionsNotification:', error);
+    console.error('Error in handleNotifyAllChannels:', error);
   }
 };
